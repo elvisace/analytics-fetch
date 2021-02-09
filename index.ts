18c1f89a556a69b9cbe5aaeaab4d320833cc5c4e
elvisace <elvisaceprog@gmail.com>
2021-02-09 09:37:47 -0600

    first commit

---
+++ index.ts
@@ -0,0 +1,66 @@
+import { AnalyticsPlugin } from "analytics";
+
+const wait = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+const fetchRetry = async (url: string, options: RequestInit, depth = 0, maxDepth = 3): Promise<void> => {
+  try {
+    await fetch(url, options);
+  } catch (err) {
+    if (depth > maxDepth) {
+      console.error(err);
+    }
+    await wait(2 ** depth * 20);
+
+    return fetchRetry(url, options, depth + 1, maxDepth);
+  }
+};
+
+type PluginConfig = {
+  url: string;
+  env: string;
+};
+
+type AnyPluginProps = {
+  payload: any;
+  config: PluginConfig;
+};
+
+// return object for analytics to use
+const postPayload = (url: string, payload: any, depth?: number, maxDepth?: number) =>
+  fetchRetry(
+    url,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(payload),
+    },
+    depth,
+    maxDepth
+  );
+
+export const analyticsPlugin = (
+  getUrl: (action: string) => string,
+  onErr?: (err: any) => any
+): AnalyticsPlugin => {
+  const plugin: AnalyticsPlugin = {
+    name: "analytics-plugin",
+    page: ({ payload }: AnyPluginProps) => {
+      const url = getUrl("page");
+      postPayload(url, payload)?.then().catch(err => onErr ? onErr(err) : null);
+    },
+    track: ({ payload }: AnyPluginProps) => {
+      const url = getUrl("track");
+      postPayload(url, payload)?.then().catch(err => onErr ? onErr(err) : null);
+    },
+    identify: ({ payload }: AnyPluginProps) => {
+      const url = getUrl("identifyk");
+      postPayload(url, payload)?.then().catch(err => onErr ? onErr(err) : null);
+    },
+    loaded: () => true,
+  };
+
+  return plugin;
+};
+
