9a573c7a8b5e7acafbc990d1b74d7165ee8584eb
elvisace <elvisaceprog@gmail.com>
2021-02-11 13:39:46 -0600

    fix: updates, thanks Quinn

--- index.ts
+++ index.ts
@@ -2,31 +2,29 @@ import { AnalyticsPlugin } from "analytics";
 
 const wait = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
-const fetchRetry = async (url: string, options: RequestInit, maxDepth = 3, depth = 0): Promise<void> => {
+const fetchRetry = async (
+  url: string,
+  options: RequestInit,
+  onErr: (err: any) => void,
+  maxDepth = 3,
+  depth = 0,
+): Promise<void> => {
   try {
     await fetch(url, options);
   } catch (err) {
     if (depth > maxDepth) {
-      console.error(err);
+      onErr(err);
+      return;
     }
     await wait(2 ** depth * 20);
-
-    return fetchRetry(url, options, maxDepth, depth + 1);
+    return fetchRetry(url, options, onErr, maxDepth, depth + 1);
   }
 };
-
-type PluginConfig = {
-  url: string;
-  env: string;
-};
-
 type AnyPluginProps = {
   payload: any;
-  config: PluginConfig;
 };
-
 // return object for analytics to use
-const postPayload = (url: string, payload: any, maxDepth?: number) =>
+const postPayload = (url: string, payload: any, onErr: (err: any) => void, maxDepth?: number) =>
   fetchRetry(
     url,
     {
@@ -36,32 +34,41 @@ const postPayload = (url: string, payload: any, maxDepth?: number) =>
       },
       body: JSON.stringify(payload),
     },
-    maxDepth
+    onErr,
+    maxDepth,
   );
-
-const analyticsFetchPlugin = (
-  getUrl: (action: string) => string,
-  maxDepth?: number,
-  onErr?: (err: any) => any
-): AnalyticsPlugin => {
-  const plugin: AnalyticsPlugin = {
-    name: "analytics-plugin",
-    page: ({ payload }: AnyPluginProps) => {
-      const url = getUrl("page");
-      postPayload(url, payload, maxDepth).then().catch(err => onErr ? onErr(err) : null);
+export type RestAnalyticsPluginProps = {
+  /**
+   * The URL to `fetch` for actions: "page", "track", and "identify"
+   */
+  getUrl: (action: string) => string;
+  /**
+   * Max number of retries
+   * @default 3
+   */
+  maxDepth?: number;
+  onOk?: (type: "page" | "track" | "identify") => void;
+  onErr?: (err: any) => void;
+};
+export const restAnalyticsPlugin = ({
+  getUrl,
+  maxDepth,
+  onOk = () => void 0,
+  onErr = console.error,
+}: RestAnalyticsPluginProps): AnalyticsPlugin => {
+  const plugin: AnalyticsPlugin = (["page", "track", "identify"] as const).reduce(
+    (p, c) => {
+      p[c] = ({ payload }: AnyPluginProps) => {
+        postPayload(getUrl(c), payload, onErr, maxDepth)
+          .then(() => onOk(c))
+          .catch((err) => (onErr ? onErr(err) : null));
+      };
+      return p;
     },
-    track: ({ payload }: AnyPluginProps) => {
-      const url = getUrl("track");
-      postPayload(url, payload, maxDepth).then().catch(err => onErr ? onErr(err) : null);
-    },
-    identify: ({ payload }: AnyPluginProps) => {
-      const url = getUrl("identify");
-      postPayload(url, payload, maxDepth).then().catch(err => onErr ? onErr(err) : null);
-    },
-    loaded: () => true,
-  };
-
+    {
+      name: "rest-analytics-plugin",
+      loaded: () => true,
+    } as AnalyticsPlugin,
+  );
   return plugin;
 };
-
-export default analyticsFetchPlugin;
